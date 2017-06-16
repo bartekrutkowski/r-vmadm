191953fb1c314f0f66feba4c6ee87120db8087b2
Heinz N. Gies <heinz@licenser.net>
2017-06-16 21:10:57 +0200

    Trivial index implementation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 #[macro_use]
 extern crate clap;
 
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
+
+use std::path::Path;
+
+use serde_json::Error;
+
+mod jdb;
+use jdb::JDB;
+
 fn main() {
     use clap::App;
     let yaml = load_yaml!("cli.yml");
@@ -8,13 +20,17 @@ fn main() {
     let matches = App::from_yaml(yaml).version(crate_version!()).get_matches();
     if matches.is_present("startup") {
         match matches.subcommand() {
-            ("", None) =>println!("startup"),
-            _ => println!("Can not use startup with a subcommand")
+            ("", None) => println!("startup"),
+            _ => println!("Can not use startup with a subcommand"),
         }
-
     } else {
         match matches.subcommand() {
-            ("list", Some(list_matches)) => println!("list jails"),
+            ("list", Some(list_matches)) => {
+                match JDB::open(Path::new("/etc/jails/index")) {
+                    Err(e) => println!("cound not open index: {}", e),
+                    Ok(db) => db.print(),
+                }
+            }
             ("create", Some(create_matches)) => println!("create jail"),
             ("update", Some(update_matches)) => println!("update jail"),
             ("destroy", Some(destroy_matches)) => println!("destroy jail"),
@@ -24,5 +40,4 @@ fn main() {
             _ => unreachable!(),
         };
     };
-    
 }
