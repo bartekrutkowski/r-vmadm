d31c26f692aa03942232aaf1c036d35495b5ac1a
Heinz N. Gies <heinz@licenser.net>
2017-06-17 03:13:55 +0200

    Add via create and check for conflicts

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,36 @@ extern crate clap;
 extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
+extern crate uuid;
 
 use std::path::Path;
+use std::io::{self, Read};
 
-use serde_json::Error;
+use std::error::Error;
+use std::fmt;
 
 mod jdb;
-use jdb::JDB;
+use jdb::{JDB, Config};
+
+static INDEX: &'static str = "/etc/jails/index";
+
+
+#[derive(Debug)]
+struct ConflictError {
+    uuid: String,
+}
+
+impl fmt::Display for ConflictError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Duplicated UUID:{}", self.uuid)
+    }
+}
+impl Error for ConflictError {
+    fn description(&self) -> &str {
+        "Conflict"
+    }
+}
+
 
 fn main() {
     use clap::App;
@@ -26,12 +49,15 @@ fn main() {
     } else {
         match matches.subcommand() {
             ("list", Some(list_matches)) => {
-                match JDB::open(Path::new("/etc/jails/index")) {
+                match JDB::open(Path::new(INDEX)) {
                     Err(e) => println!("cound not open index: {}", e),
                     Ok(db) => db.print(),
                 }
             }
-            ("create", Some(create_matches)) => println!("create jail"),
+            ("create", Some(create_matches)) => {
+                let conf = create();
+                println!("create jail: {:?}", conf);
+            }
             ("update", Some(update_matches)) => println!("update jail"),
             ("destroy", Some(destroy_matches)) => println!("destroy jail"),
             ("start", Some(destroy_matches)) => println!("start jail"),
@@ -40,4 +66,13 @@ fn main() {
             _ => unreachable!(),
         };
     };
+}
+
+fn create() -> Result<jdb::Config, Box<Error>> {
+    let mut db = JDB::open(Path::new(INDEX))?;
+    let conf: jdb::Config = serde_json::from_reader(io::stdin())?;
+    match db.find(conf.uuid.clone()) {
+        None => db.insert(conf),
+        Some(_) => Err(Box::new(ConflictError { uuid: conf.uuid })),
+    }
 }
